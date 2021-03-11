6e5dc36f230a99b69cf6a8285a4ed408f5442886
Radosław Osytek <Osytek96@gmail.com>
2021-03-11 07:27:30 +0100

    Add javascript code to articles

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
     console.log('links:', links);
   }); */
 const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
 
@@ -24,10 +25,15 @@ for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
 }
     /* get 'href' attribute from the clicked link */
+const articleSelector = clickedElement.getAttribute("href");
+console.log(articleSelector); 
+
   
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+const targetArticle = document.querySelector(articleSelector);
+console.log(targetArticle);
     /* add class 'active' to the correct article */
+targetArticle.classList.add('active');
 }
   
 const links = document.querySelectorAll('.titles a');
