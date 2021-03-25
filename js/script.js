4a63fa1b5e1a934f5116c915d4494c7e526bf97b
Radosław Osytek <Osytek96@gmail.com>
2021-03-25 15:58:27 +0100

    Finish tags and work on calculate

--- js/script.js
+++ js/script.js
@@ -78,8 +78,8 @@ function generateTitleLinks(customSelector = ''){
 generateTitleLinks(); 
 
 function generateTags(){
-    /* [NEW] create a new variable allTags with an empty array */
-    let allTags = [];
+    /* [NEW] create a new variable allTags with an empty object */
+    let allTags = {};
     /* find all articles */
     const articles = document.querySelectorAll('.post');
     /* START LOOP: for every article: */
@@ -100,10 +100,12 @@ function generateTags(){
         let tagHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>' + html;
         /* add generated code to html variable */
         tagWrapper.insertAdjacentHTML('beforeend',tagHTML);
-        /* [NEW] check if this link is NOT already in allTags */
-      if(allTags.indexOf(linkHTML) == -1){
-      /* [NEW] add generated code to allTags array */
-        allTags.push(linkHTML);
+      /* [NEW] check if this link is NOT already in allTags */
+      if(!allTags[tag]) {
+        /* [NEW] add tag to allTags object */
+        allTags[tag] = 1;
+      } else {
+        allTags[tag]++;
       }
 
       /* END LOOP: for each tag */
@@ -113,13 +115,27 @@ function generateTags(){
   
     /* END LOOP: for every article: */
     }
-      /* [NEW] find list of tags in right column */
-      const tagList = document.querySelector('.tags');
+    /* [NEW] find list of tags in right column */
+    const tagList = document.querySelector('.tags');
+    const tagsParams = calculateTagsParams(allTags);
+    console.log('tagsParams:', tagsParams)
+    const params = tagsParams.querySelectorAll(tags)
+    /* [NEW] create variable for all links HTML code */
+    
+    let allTagsHTML = '';
 
-      /* [NEW] add html from allTags to tagList */
-      tagList.innerHTML = allTags.join(' ');
+    /* [NEW] START LOOP: for each tag in allTags: */
+    for(let tag in allTags){
+      /* [NEW] generate code of a link and add it to allTagsHTML */
+      allTagsHTML += '<li><a href="#tag-' + tag + '">' + ( tag ) + '</a></li>';
+    }
+    /* [NEW] END LOOP: for each tag in allTags: */
 
+    /*[NEW] add HTML from allTagsHTML to tagList */
+    tagList.innerHTML = allTagsHTML;
+    calculateTagsParams(allTags);
 }
+  
   generateTags();
   function tagClickHandler(event){
     /* prevent default action for this event */
